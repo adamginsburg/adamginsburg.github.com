3bdafd2a42d2a4de892569abc010c329ae550da6
Adam Ginsburg <keflavich@gmail.com>
2011-11-27 18:51:32 -0700

    updated navbar!!!

--- navbar.php
+++ navbar.php
@@ -1,20 +1,17 @@
 <?php
-echo "<center><p class=navbar><nobr>[&nbsp;<a class=navbar href=\"index.htm\">Home</a>&nbsp;]</nobr>";
-echo "<nobr>[&nbsp;<a class=navbar href=\"interests.htm\">Research</a>&nbsp;]</nobr>";
-#echo "<nobr>[&nbsp;<a class=navbar href=\"astronomy.htm\">Astronomy</a>&nbsp;]</nobr>";
-#echo "<nobr>[&nbsp;<a class=navbar href=\"http://casa.colorado.edu/~bally/ASTR3520/public/\" >Astronomy&nbsp;3520&nbsp;-&nbsp;Fall&nbsp;2007</a>&nbsp;]</nobr>";
-#<!--webbot bot="Navigation" i-checksum="14968" endspan --></p>
-echo "<nobr>[&nbsp;<a class=navbar href=\"teaching.htm\">Teaching</a>&nbsp;]</nobr>";
-#echo "<nobr>[&nbsp;<a class=navbar href=\"nicfps.htm\">NICFPS page</a>&nbsp;]</nobr>";
-echo "<nobr>[&nbsp;<a class=navbar href=\"outreach.htm\">Outreach</a>&nbsp;]</nobr>";
-echo "<nobr>[&nbsp;<a class=navbar href=\"software.htm\">Software</a>&nbsp;]</nobr>";
-#echo "<nobr>[&nbsp;<a class=navbar href=\"unix_commands.htm\">Unix Command Reference</a>&nbsp;]</nobr>";
-echo "<nobr>[&nbsp;<a class=navbar href=\"contact.htm\">Contact Me</a>&nbsp;]</nobr>";
-echo "<nobr>[&nbsp;<a class=navbar href=\"about.htm\">About Me</a>&nbsp;]</nobr>";
-echo "<nobr>[&nbsp;<a class=navbar href=\"cv.htm\">CV</a>&nbsp;]</nobr>";
-#echo "<nobr>[&nbsp;<a class=navbar href=\"calendar.htm\">Calendar</a>&nbsp;]</nobr>";
-echo "<nobr>[&nbsp;<a class=navbar href=\"sitemap.htm\">Site Map</a>&nbsp;]</nobr></p>";
-#echo "<br><br>Test:".$page."<br>;
+echo "<center>";
+$links = array('Home'=>'index.htm', 'Research'=>'interests.htm', 'Teaching'=>'teaching.htm', 'Outreach'=>'outreach.htm','Software'=>'software.htm','Contact Me'=>'contact.htm','About Me'=>'about.htm','CV'=>'cv.htm','Site Map'=>'sitemap.htm');
+$leftbrace = "<span class=leftbraces>[</span>";
+$rightbrace = "<span class=rightbraces>]</span>";
+echo "<nav>";
+echo "<ul>";
+foreach ($links as $key => $value) {
+    echo "<li><a class=navbar href=\"$value\">$key</a></li>";
+}
+echo "</ul>";
+echo "</nav>";
+
+echo "</center>";
 
 echo "<script type=\"text/javascript\">";
 echo "var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");";
