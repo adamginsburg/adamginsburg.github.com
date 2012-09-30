8cacd1e6ee432e267d87ef514c665078db1d26cc
Adam Ginsburg <keflavich@gmail.com>
2012-09-29 21:23:46 -0500

    formatting fixes

--- navbar.php
+++ navbar.php
@@ -1,5 +1,5 @@
 <?php
-echo "<center>";
+#echo "<center>";
 $links = array('Home'=>'index.htm', 'Research'=>'interests.htm', 'Teaching'=>'teaching.htm', 'Outreach'=>'outreach.htm','Software'=>'software.htm','Contact Me'=>'contact.htm','CV'=>'cv.htm','Site Map'=>'sitemap.htm', "Research Blog"=>"http://adamginsburg.blogspot.com/");
 # 'About Me'=>'about.htm',
 #$links = array('Home'=>'index.htm', 'Research'=>'interests.htm', 'Teaching'=>'teaching.htm', 'Outreach'=>'outreach.htm','Software'=>'software.htm','Contact Me'=>'contact.htm','About Me'=>'about.htm','CV'=>'cv.htm','Site Map'=>'sitemap.htm');
@@ -13,7 +13,7 @@ foreach ($links as $key => $value) {
 echo "</ul>";
 echo "</nav>";
 
-echo "</center>";
+#echo "</center>";
 
 echo "<script type=\"text/javascript\">";
 echo "var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");";
