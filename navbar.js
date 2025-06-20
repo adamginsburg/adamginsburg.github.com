63bb25f73eb6c4e101c7cf593ac1b96f2948af19
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2025-06-20 18:19:27 -0400

    tweaked navbar

--- navbar.js
+++ navbar.js
@@ -3,34 +3,34 @@ function navbar () {
     return '<nav> \
 <ul class="navbar"> \
 <li> \
-<a class="navbar" href="index.htm">Home</a> \
+<a class="navbar" href="index.htm"><span style="max-width:52px" class="navbar">Home &amp; News</span></a> \
 </li> \
 <li> \
-<a class="navbar" href="researchgroup.htm">Research Group</a> \
+<a class="navbar" href="researchgroup.htm"><span class="navbar">Group  Members</span></a> \
 </li> \
 <li> \
-<a class="navbar" href="research_highlights.htm">Research Highlights</a> \
+<a class="navbar" href="research_highlights.htm"><span class="navbar">Group  Research</span></a> \
 </li> \
 <li> \
-<a class="navbar" href="interests.htm">Research</a> \
+<a class="navbar" href="interests.htm"><span class="navbar">Research  Interests</span></a> \
 </li> \
 <li> \
-<a class="navbar" href="teaching.htm">Teaching</a> \
+<a class="navbar" href="teaching.htm"><span style="max-width:61px" class="navbar">Teaching</span></a> \
 </li> \
 <li> \
-<a class="navbar" href="software.htm">Software</a> \
+<a class="navbar" href="software.htm"><span style="max-width:61px" class="navbar">Software</span></a> \
 </li> \
 <li> \
-<a class="navbar" href="https://keflavich.github.io/talks/">Talks</a> \
+<a class="navbar" href="https://keflavich.github.io/talks/"><span class="navbar">Talks</span></a> \
 </li> \
 <li> \
-<a class="navbar" href="contact.htm">Contact Me</a> \
+<a class="navbar" href="contact.htm"><span class="navbar">Contact Me</span></a> \
 </li> \
 <li> \
-<a class="navbar" href="cv.htm">CV</a> \
+<a class="navbar" href="cv.htm"><span class="navbar">CV</span></a> \
 </li> \
 <li> \
-<a class="navbar" href="https://keflavich.github.io/blog/">Research Blog</a> \
+<a class="navbar" href="https://keflavich.github.io/blog/"><span class="navbar">Research Blog</span></a> \
 </li> \
 </ul> \
 </nav>'
